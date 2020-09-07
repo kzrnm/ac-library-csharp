b6dad497673d80d81a8ddcb23d60128d1c6e93cd
keymoon <kymn0116@gmail.com>
2020-09-08 05:55:41 +0900

    add DSU

---
+++ AtCoderLibrary/DSU.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+
+namespace AtCoder
+{
+    class DSU
+    {
+        /// <summary>
+        /// <see cref="DSU"/> クラスの新しいインスタンスを、<paramref name="n"/> 頂点 0 辺の有向グラフとして初期化します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="n"/>≤10^8</para>
+        /// <para>計算量: O(<paramref name="n"/>)</para>
+        /// </remarks>
+        public DSU(int n) { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// 頂点 <paramref name="a"/> と頂点 <paramref name="b"/> を結ぶ辺を追加し、それらの代表元を返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="a"/>, <paramref name="b"/>&lt;n</para>
+        /// <para>計算量: ならしO(a(n))</para>
+        /// </remarks>
+        public int Merge(int a, int b) { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// 頂点 <paramref name="a"/>, <paramref name="b"/> が連結かどうかを返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="a"/>, <paramref name="b"/>&lt;n</para>
+        /// <para>計算量: ならしO(a(n))</para>
+        /// </remarks>
+        public bool Same(int a, int b) { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// 頂点 <paramref name="a"/> の属する連結成分の代表元を返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="a"/>&lt;n</para>
+        /// <para>計算量: ならしO(a(n))</para>
+        /// </remarks>
+        public bool Leader(int a) { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// 頂点 <paramref name="a"/> の属する連結成分のサイズを返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="a"/>&lt;n</para>
+        /// <para>計算量: ならしO(a(n))</para>
+        /// </remarks>
+        public int Size(int a) { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// グラフを連結成分に分け、その情報を返します。
+        /// </summary>
+        /// <para>計算量: O(n)</para>
+        /// <returns>「一つの連結成分の頂点番号のリスト」のリスト。</returns>
+        public List<List<int>> Groups() { throw new Exception(); }
+    }
+}
