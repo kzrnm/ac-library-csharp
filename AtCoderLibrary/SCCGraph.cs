353e7227804aac3f320f55d95e93328e029e69ac
keymoon <kymn0116@gmail.com>
2020-09-08 05:29:01 +0900

    add scc

---
+++ AtCoderLibrary/SCCGraph.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtCoder
+{
+    /// <summary>
+    /// 有向グラフを強連結成分分解します。
+    /// </summary>
+    public class SCCGraph
+    {
+        /// <summary>
+        /// <see cref="SCCGraph"/> クラスの新しいインスタンスを、<paramref name="n"/> 頂点 0 辺の有向グラフとして初期化します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="n"/>≤10^8</para>
+        /// <para>計算量: O(<paramref name="n"/>)</para>
+        /// </remarks>
+        public SCCGraph(int n) { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// 頂点 <paramref name="from"/> から頂点 <paramref name="to"/> へ有向辺を追加します。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0≤<paramref name="from"/>, <paramref name="to"/>≤n</para>
+        /// <para>計算量: ならしO(1)</para>
+        /// </remarks>
+        public void AddEdge(int from, int to) { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// 強連結成分分解の結果である「頂点のリスト」のリストを取得します。
+        /// </summary>
+        /// <remarks>
+        /// <para>- 全ての頂点がちょうど1つずつ、どれかのリストに含まれます。</para>
+        /// <para>- 内側のリストと強連結成分が一対一に対応します。リスト内での頂点の順序は未定義です。</para>
+        /// <para>- リストはトポロジカルソートされています。異なる強連結成分の頂点 u, v について、u から v に到達できる時、u の属するリストは v の属するリストよりも前です。</para>
+        /// <para>計算量: 追加された辺の本数を m として O(n+m)</para>
+        /// </remarks>
+        public List<List<int>> GetSCC() { throw new Exception(); }
+    }
+}
