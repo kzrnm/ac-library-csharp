396f2946e8f6e7d2c66582cf8aeed27dbb620fbe
keymoon <kymn0116@gmail.com>
2020-09-08 12:07:40 +0900

    add pow mod

--- AtCoderLibrary/Math/PowMod.cs
+++ AtCoderLibrary/Math/PowMod.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AtCoder.Internal;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace AtCoder
@@ -13,6 +15,19 @@ namespace AtCoder
         /// <para>制約: 0≤<paramref name="n"/>, 1≤<paramref name="m"/></para>
         /// <para>計算量: O(log<paramref name="n"/>)</para>
         /// </remarks>
-        public static long PowMod(long x, long n, int m) { throw new NotImplementedException(); }
+        public static long PowMod(long x, long n, int m)
+        {
+            Debug.Assert(0 <= n && 1 <= m);
+            if (m == 1) return 0;
+            Barrett barrett = new Barrett((uint)m);
+            uint r = 1, y = (uint)InternalMath.SafeMod(x, m);
+            while (0 < n)
+            {
+                if ((n & 1) != 0) r = barrett.Mul(r, y);
+                y = barrett.Mul(y, y);
+                n >>= 1;
+            }
+            return r;
+        }
     }
 }
