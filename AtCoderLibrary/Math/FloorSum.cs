0562595d3bdb1dd150512dcf95211f78eafda25e
keymoon <kymn0116@gmail.com>
2020-09-08 14:23:26 +0900

    add FloorSum

--- AtCoderLibrary/Math/FloorSum.cs
+++ AtCoderLibrary/Math/FloorSum.cs
@@ -14,6 +14,28 @@ namespace AtCoder
         /// <para>計算量: O(log(n+m+a+b))</para>
         /// </remarks>
         /// <returns></returns>
-        public static long FloorSum(long n, long m, long a, long b) { throw new NotImplementedException(); }
+        public static long FloorSum(long n, long m, long a, long b) 
+        {
+            long ans = 0;
+            while (true)
+            {
+                if (a >= m)
+                {
+                    ans += (n - 1) * n * (a / m) / 2;
+                    a %= m;
+                }
+                if (b >= m)
+                {
+                    ans += n * (b / m);
+                    b %= m;
+                }
+
+                long yMax = (a * n + b) / m;
+                long xMax = yMax * m - b;
+                if (yMax == 0) return ans;
+                ans += (n - (xMax + a - 1) / a) * yMax;
+                (n, m, a, b) = (yMax, a, m, (a - xMax % a) % a);
+            }
+        }
     }
 }
