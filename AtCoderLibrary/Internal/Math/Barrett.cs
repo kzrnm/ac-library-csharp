0c3042a7f2aa3329592fb0589e1b7a43fd60e010
keymoon <kymn0116@gmail.com>
2020-09-08 12:07:34 +0900

    add barret reduction and safe mod

---
+++ AtCoderLibrary/Internal/Math/Barrett.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Intrinsics.X86;
+using System.Runtime.Intrinsics;
+using System.Numerics;
+
+namespace AtCoder.Internal
+{
+    /// <summary>
+    /// Fast moduler by barrett reduction
+    /// <seealso href="https://en.wikipedia.org/wiki/Barrett_reduction"/>
+    /// </summary>
+    public class Barrett
+    {
+        public uint Mod { get; private set; }
+        private ulong IM;
+        public Barrett(uint m)
+        {
+            Mod = m;
+            IM = unchecked((ulong)-1) / m + 1;
+        }
+
+        /// <summary>
+        /// <paramref name="a"/> * <paramref name="b"/> mod m
+        /// </summary>
+        public uint Mul(uint a, uint b)
+        {
+            ulong z = a;
+            z *= b;
+            if (!Bmi2.X64.IsSupported) return (uint)(z % Mod);
+            var x = Bmi2.X64.MultiplyNoFlags(z, IM);
+            var v = unchecked((uint)(z - x * Mod));
+            if (Mod <= v) v += Mod;
+            return v;
+        }
+    }
+}
---
+++ AtCoderLibrary/Internal/Math/SafeMod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtCoder.Internal
+{
+    public static partial class InternalMath
+    {
+        public static long SafeMod(long x, long m)
+        {
+            x %= m;
+            if (x < 0) x += m;
+            return x;
+        }
+    }
+}
