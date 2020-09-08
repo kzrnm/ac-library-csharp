2b8ac05c75e9d4665d90cd345dd494d55bef147e
keymoon <kymn0116@gmail.com>
2020-09-08 09:35:53 +0900

    implement DSU

--- AtCoderLibrary/DSU.cs
+++ AtCoderLibrary/DSU.cs
@@ -1,22 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
-using MethodImplOptions = System.Runtime.CompilerServices.MethodImplOptions;
+using System.Diagnostics;
 
 namespace AtCoder
 {
     class DSU
     {
+        private int Count;
+        private int[] ParentOrSize;
+
         /// <summary>
-        /// <see cref="DSU"/> クラスの新しいインスタンスを、<paramref name="n"/> 頂点 0 辺の有向グラフとして初期化します。
+        /// <see cref="DSU"/> クラスの新しいインスタンスを、<paramref name="n"/> 頂点 0 辺のグラフとして初期化します。
         /// </summary>
         /// <remarks>
         /// <para>制約: 0≤<paramref name="n"/>≤10^8</para>
         /// <para>計算量: O(<paramref name="n"/>)</para>
         /// </remarks>
-        public DSU(int n) { throw new NotImplementedException(); }
+        public DSU(int n)
+        {
+            Count = n;
+            ParentOrSize = new int[n];
+            for (int i = 0; i < ParentOrSize.Length; i++) ParentOrSize[i] = -1;
+        }
 
         /// <summary>
         /// 頂点 <paramref name="a"/> と頂点 <paramref name="b"/> を結ぶ辺を追加し、それらの代表元を返します。
@@ -25,7 +29,17 @@ namespace AtCoder
         /// <para>制約: 0≤<paramref name="a"/>, <paramref name="b"/>&lt;n</para>
         /// <para>計算量: ならしO(a(n))</para>
         /// </remarks>
-        public int Merge(int a, int b) { throw new NotImplementedException(); }
+        public int Merge(int a, int b)
+        {
+            Debug.Assert(0 <= a && a < Count);
+            Debug.Assert(0 <= b && b < Count);
+            int x = Leader(a), y = Leader(b);
+            if (x == y) return x;
+            if (-ParentOrSize[x] < ParentOrSize[y]) (x, y) = (y, x);
+            ParentOrSize[x] += ParentOrSize[y];
+            ParentOrSize[y] = x;
+            return x;
+        }
 
         /// <summary>
         /// 頂点 <paramref name="a"/>, <paramref name="b"/> が連結かどうかを返します。
@@ -34,7 +48,12 @@ namespace AtCoder
         /// <para>制約: 0≤<paramref name="a"/>, <paramref name="b"/>&lt;n</para>
         /// <para>計算量: ならしO(a(n))</para>
         /// </remarks>
-        public bool Same(int a, int b) { throw new NotImplementedException(); }
+        public bool Same(int a, int b)
+        {
+            Debug.Assert(0 <= a && a < Count);
+            Debug.Assert(0 <= b && b < Count);
+            return Leader(a) == Leader(b);
+        }
 
         /// <summary>
         /// 頂点 <paramref name="a"/> の属する連結成分の代表元を返します。
@@ -43,7 +62,16 @@ namespace AtCoder
         /// <para>制約: 0≤<paramref name="a"/>&lt;n</para>
         /// <para>計算量: ならしO(a(n))</para>
         /// </remarks>
-        public bool Leader(int a) { throw new NotImplementedException(); }
+        public int Leader(int a)
+        {
+            if (ParentOrSize[a] < 0) return a;
+            while (0 <= ParentOrSize[ParentOrSize[a]])
+            {
+                (a, ParentOrSize[a]) = (ParentOrSize[a], ParentOrSize[ParentOrSize[a]]);
+            }
+            return ParentOrSize[a];
+        }
+
 
         /// <summary>
         /// 頂点 <paramref name="a"/> の属する連結成分のサイズを返します。
@@ -52,13 +80,42 @@ namespace AtCoder
         /// <para>制約: 0≤<paramref name="a"/>&lt;n</para>
         /// <para>計算量: ならしO(a(n))</para>
         /// </remarks>
-        public int Size(int a) { throw new NotImplementedException(); }
+        public int Size(int a)
+        {
+            Debug.Assert(0 <= a && a < Count);
+            return -ParentOrSize[Leader(a)];
+        }
 
         /// <summary>
         /// グラフを連結成分に分け、その情報を返します。
         /// </summary>
         /// <para>計算量: O(n)</para>
         /// <returns>「一つの連結成分の頂点番号のリスト」のリスト。</returns>
-        public List<List<int>> Groups() { throw new Exception(); }
+        public Span<int[]> Groups()
+        {
+            int[] leaderBuf = new int[Count];
+            int[] id = new int[Count];
+            Span<int[]> result = new int[Count][];
+            int groupCount = 0;
+            for (int i = 0; i < leaderBuf.Length; i++)
+            {
+                leaderBuf[i] = Leader(i);
+                if (i == leaderBuf[i])
+                {
+                    id[i] = groupCount;
+                    result[id[i]] = new int[-ParentOrSize[i]];
+                    groupCount++;
+                }
+            }
+            int[] ind = new int[groupCount];
+            result = result.Slice(0, groupCount);
+            for (int i = 0; i < leaderBuf.Length; i++)
+            {
+                var leaderID = id[leaderBuf[i]];
+                result[leaderID][ind[leaderID]] = i;
+                ind[leaderID]++;
+            }
+            return result;
+        }
     }
 }
