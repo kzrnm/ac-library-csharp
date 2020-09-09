74ebdfe91decf6907da2c559c0c1cfa5552de786
naminodarie <gengesa@gmail.com>
2020-09-09 21:34:48 +0900

    実行ファイルを作成

---
+++ AtCoderProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using AtCoder;
+using static AtCoder.Math;
+using static AtCoder.String;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(AtCoder.Math.PowMod(11, 3131, 13));
+    }
+}
