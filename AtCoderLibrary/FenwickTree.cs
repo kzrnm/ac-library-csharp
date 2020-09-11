c029241c6bc360f54992d361322f2cc29818ab99
naminodarie <gengesa@gmail.com>
2020-09-11 22:53:19 +0900

    Add FenwickTree

---
+++ AtCoderLibrary/FenwickTree.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using AtCoder.Internal;
+
+namespace AtCoder
+{
+    public class IntFenwickTree : FenwickTree<int, IntOperator> { public IntFenwickTree(int n) : base(n) { } }
+    public class UIntFenwickTree : FenwickTree<uint, UIntOperator> { public UIntFenwickTree(int n) : base(n) { } }
+    public class LongFenwickTree : FenwickTree<long, LongOperator> { public LongFenwickTree(int n) : base(n) { } }
+    public class ULongFenwickTree : FenwickTree<ulong, ULongOperator> { public ULongFenwickTree(int n) : base(n) { } }
+    // TODO: public class ModIntFenwickTree : FenwickTree<ModInt, ModIntOperator> { public ModIntFenwickTree(int n) : base(n) { } }
+
+
+    public class FenwickTree<TValue, TOp>
+        where TValue : struct
+        where TOp : struct, INumOperator<TValue>
+    {
+        private static readonly TOp op = default;
+        private readonly TValue[] data;
+        public FenwickTree(int n)
+        {
+            data = new TValue[n + 1];
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Add(int p, TValue x)
+        {
+            Debug.Assert(unchecked((uint)p < data.Length));
+            for (p++; p < data.Length; p += InternalBit.ExtractLowestSetBit(p))
+            {
+                data[p] = op.Add(data[p], x);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public TValue Sum(int l, int r)
+        {
+            Debug.Assert(0 <= l && l <= r && r < data.Length);
+            return op.Subtract(Sum(r), Sum(l));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private TValue Sum(int r)
+        {
+            TValue s = default;
+            for (; r > 0; r -= InternalBit.ExtractLowestSetBit(r))
+            {
+                s = op.Add(s, data[r]);
+            }
+            return s;
+        }
+    }
+}
---
+++ AtCoderLibrary/Internal/InternalBit.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.X86;
+
+namespace AtCoder.Internal
+{
+    public static class InternalBit
+    {
+        /// <summary>
+        /// _blsi_u32 OR <paramref name="n"/> &amp; -<paramref name="n"/>
+        /// <para><paramref name="n"/>で立っているうちの最下位の 1 ビットのみを立てた整数を返す</para>
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns><paramref name="n"/> &amp; -<paramref name="n"/></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int ExtractLowestSetBit(int n)
+        {
+            if (Bmi1.IsSupported)
+            {
+                return (int)Bmi1.ExtractLowestSetBit((uint)n);
+            }
+            return n & -n;
+        }
+    }
+}
