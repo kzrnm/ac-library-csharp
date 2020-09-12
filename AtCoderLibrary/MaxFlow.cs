f405d3ec29a9c40c7fad71d2c368b7aca92cb503
takytank <taki_taki_taky@hotmail.co.jp>
2020-09-13 00:22:20 +0900

    add MinFlow comments

--- AtCoderLibrary/MaxFlow.cs
+++ AtCoderLibrary/MaxFlow.cs
@@ -3,16 +3,42 @@ using System.Diagnostics;
 
 namespace AtCoder
 {
+    /// <summary>
+    /// 最大フロー問題 を解くライブラリ(int版)です。
+    /// </summary>
     public class MFGraphInt : MFGraph<int, IntOperator> { public MFGraphInt(int n) : base(n) { } }
-    public class MFGraphUInt : MFGraph<uint, UIntOperator> { public MFGraphUInt(int n) : base(n) { } }
+
+    /// <summary>
+    /// 最大フロー問題 を解くライブラリ(long版)です。
+    /// </summary>
     public class MFGraphLong : MFGraph<long, LongOperator> { public MFGraphLong(int n) : base(n) { } }
-    public class MFGraphULong : MFGraph<ulong, ULongOperator> { public MFGraphULong(int n) : base(n) { } }
-    public class MFGraphDouble : MFGraph<double, DoubleOperator> { public MFGraphDouble(int n) : base(n) { } }
+
+    /// <summary>
+    /// 最大フロー問題 を解くライブラリです。
+    /// </summary>
+    /// <typeparam name="TValue">容量の型</typeparam>
+    /// <typeparam name="TOp"><typeparamref name="TValue"/>に対応する演算を提要する型</typeparam>
+    /// <remarks>
+    /// <para>制約: <typeparamref name="TValue"/> は int, long。</para>
+    /// <para>
+    /// 内部では各辺 e について 2 つの変数、流量 f_e と容量 c_e を管理しています。
+    /// 頂点 v から出る辺の集合を out(v)、入る辺の集合を in(v)、
+    /// また頂点 v について g(v, f) = (Σ_in(v) f_e) - (Σ_out(v) f_e) とします。 
+    /// </para>
+    /// </remarks>
     public class MFGraph<TValue, TOp>
          where TValue : struct
          where TOp : struct, INumOperator<TValue>
     {
         static readonly TOp op = default;
+
+        /// <summary>
+        /// <paramref name="n"/> 頂点 0 辺のグラフを作ります。
+        /// </summary>
+        /// <remarks>
+        /// <para>制約: 0 ≤ <paramref name="n"/> ≤ 10^8</para>
+        /// <para>計算量: O(<paramref name="n"/>)</para>
+        /// </remarks>
         public MFGraph(int n)
         {
             _n = n;
@@ -24,6 +50,22 @@ namespace AtCoder
             _pos = new List<(int first, int second)>();
         }
 
+        /// <summary>
+        /// <paramref name="from"/> から <paramref name="to"/> へ
+        /// 最大容量 <paramref name="cap"/>、流量 0 の辺を追加し、何番目に追加された辺かを返します。
+        /// </summary>
+        /// <remarks>
+        /// 制約: 
+        /// <list type="bullet">
+        /// <item>
+        /// <description>0 ≤ <paramref name="from"/>, <paramref name="to"/> &lt; n</description>
+        /// </item>
+        /// <item>
+        /// <description>0 ≤ <paramref name="cap"/></description>
+        /// </item>
+        /// </list>
+        /// <para>計算量: ならしO(1)</para>
+        /// </remarks>
         public int AddEdge(int from, int to, TValue cap)
         {
             int m = _pos.Count;
@@ -36,6 +78,13 @@ namespace AtCoder
             return m;
         }
 
+        /// <summary>
+        /// 今の内部の辺の状態を返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>AddEdge で <paramref name="i"/> 番目 (0-indexed) に追加された辺を返す。</para>
+        /// <para>計算量: O(1)</para>
+        /// </remarks>
         public Edge GetEdge(int i)
         {
             int m = _pos.Count;
@@ -45,6 +94,13 @@ namespace AtCoder
             return new Edge(_pos[i].first, _e.To, op.Add(_e.Cap, _re.Cap), _re.Cap);
         }
 
+        /// <summary>
+        /// 今の内部の辺の状態を返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>辺の順番はadd_edgeで追加された順番と同一。</para>
+        /// <para>計算量: O(n)</para>
+        /// </remarks>
         public List<Edge> Edges()
         {
             int m = _pos.Count;
@@ -56,6 +112,17 @@ namespace AtCoder
             return result;
         }
 
+        /// <summary>
+        /// <paramref name="i"/> 番目に追加された辺の容量、流量を
+        /// <paramref name="newCap"/>, <paramref name="newFlow"/> に変更します。
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// 他の辺の容量、流量は変更しません。
+        /// 辺 <paramref name="i"/> の流量、容量のみを
+        /// <paramref name="newCap"/>, <paramref name="newFlow"/> へ変更します。
+        /// </para>
+        /// </remarks>
         public void ChangeEdge(int i, TValue newCap, TValue newFlow)
         {
             int m = _pos.Count;
@@ -67,11 +134,94 @@ namespace AtCoder
             _re.Cap = newFlow;
         }
 
+        /// <summary>
+        /// 頂点 <paramref name="s"/> から <paramref name="t"/> へ流せる限り流し、
+        /// 流せた量を返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// 複数回呼ぶことも可能で、その時の挙動は
+        /// 変更前と変更後の流量を f_e, f'_e として、以下の条件を満たすように変更します。
+        /// </para>
+        /// <list type="bullet">
+        /// <item>
+        /// <description>0 ≤ f'_e ≤ C_e</description>
+        /// </item>
+        /// <item>
+        /// <description>
+        /// <paramref name="s"/>, <paramref name="t"/> 以外の頂天 v について、
+        /// g(v, f) = g(v, f')
+        /// </description>
+        /// </item>
+        /// <item>
+        /// <description>
+        /// (flowLimit を指定した場合) g(t, f') - g(t, f) ≤ flowLimit
+        /// </description>
+        /// </item>
+        /// <item>
+        /// <description>
+        /// g(t, f') - g(t, f) が委譲の条件を満たすうち最大。この g(t, f') - g(t, f) を返す。
+        /// </description>
+        /// </item>
+        /// </list>
+        /// <para>制約: 返値が <typeparamref name="TValue"/> に収まる。</para>
+        /// 計算量: m を追加された辺数として、
+        /// <list type="bullet">
+        /// <item>
+        /// <description>O(min(n^(2/3) m, m^(3/2))) (辺の容量が全部 1 の時)</description>
+        /// </item>
+        /// <item>
+        /// <description>O(n^2 m)</description>
+        /// </item>
+        /// </list>
+        /// </remarks>
         public TValue Flow(int s, int t)
         {
             return Flow(s, t, op.MaxValue);
         }
 
+        /// <summary>
+        /// 頂点 <paramref name="s"/> から <paramref name="t"/> へ
+        /// 流量 <paramref name="flowLimit"/> に達するまで流せる限り流し、
+        /// 流せた量を返します。
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// 複数回呼ぶことも可能で、その時の挙動は
+        /// 変更前と変更後の流量を f_e, f'_e として、以下の条件を満たすように変更します。
+        /// </para>
+        /// <list type="bullet">
+        /// <item>
+        /// <description>0 ≤ f'_e ≤ C_e</description>
+        /// </item>
+        /// <item>
+        /// <description>
+        /// <paramref name="s"/>, <paramref name="t"/> 以外の頂天 v について、
+        /// g(v, f) = g(v, f')
+        /// </description>
+        /// </item>
+        /// <item>
+        /// <description>
+        /// (<paramref name="flowLimit"/> を指定した場合) g(t, f') - g(t, f) ≤ <paramref name="flowLimit"/>
+        /// </description>
+        /// </item>
+        /// <item>
+        /// <description>
+        /// g(t, f') - g(t, f) が委譲の条件を満たすうち最大。この g(t, f') - g(t, f) を返す。
+        /// </description>
+        /// </item>
+        /// </list>
+        /// <para>制約: 返値が <typeparamref name="TValue"/> に収まる。</para>
+        /// 計算量: m を追加された辺数として、
+        /// <list type="bullet">
+        /// <item>
+        /// <description>O(min(n^(2/3) m, m^(3/2))) (辺の容量が全部 1 の時)</description>
+        /// </item>
+        /// <item>
+        /// <description>O(n^2 m)</description>
+        /// </item>
+        /// </list>
+        /// </remarks>
         public TValue Flow(int s, int t, TValue flowLimit)
         {
             Debug.Assert(0 <= s && s < _n);
@@ -145,6 +295,26 @@ namespace AtCoder
             return flow;
         }
 
+        /// <summary>
+        /// 長さ n の配列を返します。
+        /// i 番目の要素には、頂点 <paramref name="s"/> から i へ残余グラフで到達可能なとき、
+        /// またその時のみ true を返します。
+        /// Flow(s, t) を flowLimit なしでちょうど一回呼んだ後に呼ぶと、
+        /// 返り値はs, t 間の mincut に対応します。
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// 各辺 e = (u, v, f_e, c_e) について、 f_e &lt; c_e ならば辺 (u, v) を張り、
+        /// 0 &lt; f_e ならば辺 (u, v) を張ったと仮定したとき、
+        /// 頂点 ss から到達可能な頂点の集合を返します。
+        /// </para>
+        /// 計算量: m を追加された辺数として、
+        /// <list type="bullet">
+        /// <item>
+        /// <description>O(n + m)</description>
+        /// </item>
+        /// </list>
+        /// </remarks>
         public bool[] MinCut(int s)
         {
             var visited = new bool[_n];
