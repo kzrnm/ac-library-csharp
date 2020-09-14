b6dd2edb454768456a957e2e11574bd6efa3ab20
naminodarie <gengesa@gmail.com>
2020-09-14 20:55:27 +0900

    add expander

---
+++ AtCoderLibrary.Expander/Expander.Sandbox/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using AtCoder;
+using static AtCoder.Internal.InternalMath;
+using FenwickTree = AtCoder.IntFenwickTree;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = Stopwatch.StartNew();
+            Expander.Expand();
+            Console.WriteLine($"expand time: {sw.ElapsedMilliseconds} ms");
+            var fw = new FenwickTree(5);
+            for (int i = 0; i < 5; i++) fw.Add(i, i + 1);
+            Console.WriteLine(fw.Sum(0, 5));
+            Console.WriteLine(CeilPow2(15));
+            Console.WriteLine($"finish time: {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
+struct Monoid : IMonoidOperator<int>
+{
+    public int Identity => 0;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int Operate(int x, int y) => x + y;
+}
---
+++ AtCoderLibrary.Expander/Expander/Internal/AclFileInfoDetail.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AtCoder.Embedded;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace AtCoder.Internal
+{
+    internal class AclFileInfoDetail
+    {
+        public string FileName { get; }
+        public IList<string> TypeNames { get; }
+        public IList<string> Usings { get; }
+        public string CodeBody { get; }
+        public SyntaxTree SyntaxTree { get; }
+        public AclFileInfoDetail(string fileName, IEnumerable<string> typeNames, IEnumerable<string> usings, string code)
+        {
+            FileName = fileName;
+            TypeNames = typeNames.ToArray();
+            Usings = usings.ToArray();
+            CodeBody = code;
+            SyntaxTree = CSharpSyntaxTree.ParseText(string.Join("\n", Usings) + "\n" + CodeBody);
+        }
+        public AclFileInfoDetail(AclFileInfo orig) : this(orig.FileName, orig.TypeNames, orig.Usings, orig.CodeBody) { }
+    }
+}
