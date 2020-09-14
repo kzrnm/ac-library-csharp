0cecebfb934f5d9d98fdff374a1b7a7137b47ef8
naminodarie <gengesa@gmail.com>
2020-09-14 20:55:27 +0900

    add expander

---
+++ AtCoderLibrary.Expander/Expander/Internal/UsingRemover.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace AtCoder.Internal
+{
+    internal class UsingRemover : CSharpSyntaxRewriter
+    {
+        public override SyntaxNode? VisitUsingDirective(UsingDirectiveSyntax node)
+            => node.Parent.IsKind(SyntaxKind.CompilationUnit) ? default : base.VisitUsingDirective(node);
+    }
+}
