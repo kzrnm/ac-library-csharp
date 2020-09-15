8c78e569e8badf186be0d3990cfbe99c0fabc69b
keymoon <kymn0116@gmail.com>
2020-09-15 21:40:21 +0900

    add tests

--- AtCoderLibrary.Test/Example/SCC.cs
+++ AtCoderLibrary.Test/Example/SCC.cs
@@ -2,39 +2,45 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using AtCoder.Test.Utils;
 using NUnit.Framework;
 
 namespace AtCoder.Test
 {
     public class SCCTest
     {
-        [Test]
-        public string Practice(string testCase)
+        [ProblemTestCase(@"https://judge.yosupo.jp/problem/scc")]
+        public void Practice(string input, string answer)
         {
-            var reader = new StringReader(testCase);
+            var reader = new StringReader(input);
             var writer = new StringWriter();
 
-            int[] nm = reader.ReadLine().Split().Select(int.Parse).ToArray();
-            (int n, int m) = (nm[0], nm[1]);
+            Solver(reader, writer);
 
-            var g = new SCCGraph(n);
+            Assert.IsTrue(new AlwaysFailValidator().IsValid(input, answer, writer.ToString()));
 
-            for (int i = 0; i < m; i++)
+            static void Solver(TextReader reader, TextWriter writer)
             {
-                int[] uv = reader.ReadLine().Split().Select(int.Parse).ToArray();
-                (int u, int v) = (uv[0], uv[1]);
-                g.AddEdge(u, v);
+                int[] nm = reader.ReadLine().Split().Select(int.Parse).ToArray();
+                (int n, int m) = (nm[0], nm[1]);
+
+                var g = new SCCGraph(n);
+
+                for (int i = 0; i < m; i++)
+                {
+                    int[] uv = reader.ReadLine().Split().Select(int.Parse).ToArray();
+                    (int u, int v) = (uv[0], uv[1]);
+                    g.AddEdge(u, v);
+                }
+
+                List<List<int>> scc = g.SCC();
+
+                writer.WriteLine(scc.Count);
+                foreach (List<int> v in scc)
+                {
+                    writer.WriteLine(string.Join(" ", v));
+                }
             }
-
-            List<List<int>> scc = g.SCC();
-
-            writer.WriteLine(scc.Count);
-            foreach (List<int> v in scc)
-            {
-                writer.WriteLine(string.Join(" ", v));
-            }
-
-            return writer.ToString();
         }
     }
 }
