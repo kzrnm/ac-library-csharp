134f5fce7152fdff5bc449b8267296c14f70656d
terry_u16 <terry-u16@users.noreply.github.com>
2020-09-15 22:41:40 +0900

    Add PowMod

---
+++ AtCoderLibrary/Internal/Math/PowMod.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+namespace AtCoder.Internal
+{
+    public static partial class InternalMath
+    {
+        public static long PowMod(long x, long n, int m)
+        {
+            Debug.Assert(n >= 0);
+            Debug.Assert(m >= 1);
+
+            if (m == 1)
+            {
+                return 0;
+            }
+
+            uint _m = (uint)m;
+            ulong r = 1;
+            ulong y = (ulong)SafeMod(x, m);
+
+            while (n > 0)
+            {
+                if ((n & 1) > 0)
+                {
+                    r = (r * y) % _m;
+                }
+
+                y = (y * y) % _m;
+                n >>= 1;
+            }
+
+            return (long)r;
+        }
+    }
+}
