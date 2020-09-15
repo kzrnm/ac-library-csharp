32335d245f6fe34073ffe58ff1be19805d4dfc2c
keymoon <kymn0116@gmail.com>
2020-09-15 21:40:05 +0900

    add validator

---
+++ AtCoderLibrary.Test/Validator/AlwaysPassValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace AtCoder.Test
+{
+    class AlwaysPassValidator : IValidator
+    {
+        public bool IsValid(string input, string answer, string output) => true;
+    }
+}
