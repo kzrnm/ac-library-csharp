4c29972a9dc626c30b603b09d0c9f6aa71df05c7
naminodarie <gengesa@gmail.com>
2020-10-08 22:10:09 +0900

    不要なnamespace

--- AtCoderLibrary/String/StringLib.cs
+++ AtCoderLibrary/String/StringLib.cs
@@ -160,7 +160,7 @@ namespace AtCoder
             for (int i = 1, j = 0; i < n; i++)
             {
                 ref int k = ref z[i];
-                k = (j + z[j] <= i) ? 0 : System.Math.Min(j + z[j] - i, z[i - j]);
+                k = (j + z[j] <= i) ? 0 : Math.Min(j + z[j] - i, z[i - j]);
                 while (i + k < n && EqualityComparer<T>.Default.Equals(s[k], s[i + k])) k++;
                 if (j + z[j] < i + z[i]) j = i;
             }
