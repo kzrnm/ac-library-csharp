2112446953c26605bfd840125cda5cc5d2dba929
naminodarie <gengesa@gmail.com>
2020-10-09 11:55:37 +0900

    add debug view class

---
+++ AtCoderLibrary/Internal/CollectionDebugView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AtCoder.Internal
+{
+    internal class CollectionDebugView<T>
+    {
+        private readonly IEnumerable<T> collection;
+        public CollectionDebugView(IEnumerable<T> collection)
+        {
+            this.collection = collection ?? throw new ArgumentNullException(nameof(collection));
+        }
+        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        public T[] Items => collection.ToArray();
+    }
+}
