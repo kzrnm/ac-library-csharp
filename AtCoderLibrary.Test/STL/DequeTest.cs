c229591df08b03475c0fc01781c78dc88b50caba
naminodarie <gengesa@gmail.com>
2020-10-10 14:50:10 +0900

    add deque

---
+++ AtCoderLibrary.Test/STL/DequeTest.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using AtCoder.Internal;
+using FluentAssertions;
+using Xunit;
+
+namespace AtCoder
+{
+    public class DequeTest
+    {
+        [Fact]
+        public void Simple()
+        {
+            var deque = new Deque<int>();
+
+            deque.data.Should().HaveCount(8);
+            deque.Should().HaveCount(0);
+
+            deque.Invoking(deque => deque.PopLast()).Should().Throw<InvalidOperationException>();
+            deque.Invoking(deque => deque.PopFirst()).Should().Throw<InvalidOperationException>();
+
+            for (int i = 1; i < 8; i++)
+            {
+                deque.AddLast(i);
+                deque.Last.Should().Be(i);
+                deque.data.Should().HaveCount(8);
+                deque.Should().HaveCount(i);
+            }
+            deque.Add(-1);
+            deque.data.Should().HaveCount(16);
+            deque.Should().HaveCount(8);
+            for (int i = deque.Count + 1; i <= 10000; i++)
+            {
+                deque.AddFirst(i);
+                deque.First.Should().Be(i);
+                deque.Last.Should().Be(-1);
+                deque.data.Should().HaveCount(1 << InternalBit.CeilPow2(i + 1));
+                deque.Should().HaveCount(i);
+            }
+
+            var cap = 1 << InternalBit.CeilPow2(10000);
+            for (int i = deque.Count - 1; i >= 8; i--)
+            {
+                deque.PopFirst().Should().Be(i + 1);
+                deque.data.Should().HaveCount(cap);
+                deque.Should().HaveCount(i);
+            }
+
+            deque.PopLast().Should().Be(-1);
+
+            for (int i = deque.Count - 1; i >= 0; i--)
+            {
+                deque.PopLast().Should().Be(i + 1);
+                deque.data.Should().HaveCount(cap);
+                deque.Should().HaveCount(i);
+            }
+
+            deque.Invoking(deque => deque.PopLast()).Should().Throw<InvalidOperationException>();
+            deque.Invoking(deque => deque.PopFirst()).Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void Contains()
+        {
+            var deque = new Deque<int>();
+            ((ICollection<int>)deque).Contains(0).Should().BeFalse();
+            deque.AddLast(1);
+            ((ICollection<int>)deque).Contains(1).Should().BeTrue();
+            deque.AddLast(1);
+            ((ICollection<int>)deque).Contains(1).Should().BeTrue();
+            deque.PopFirst();
+            ((ICollection<int>)deque).Contains(1).Should().BeTrue();
+            deque.PopFirst();
+            ((ICollection<int>)deque).Contains(1).Should().BeFalse();
+            deque.AddFirst(2);
+            ((ICollection<int>)deque).Contains(2).Should().BeTrue();
+            ((ICollection<int>)deque).Contains(1).Should().BeFalse();
+
+            for (int i = 0; i < 4; i++)
+                deque.AddFirst(i + 4);
+
+            ((ICollection<int>)deque).Contains(2).Should().BeTrue();
+            ((ICollection<int>)deque).Contains(7).Should().BeTrue();
+        }
+
+        [Fact]
+        public void CopyTo()
+        {
+            var dist = new int[8];
+            dist.AsSpan().Fill(-1);
+            var deque = new Deque<int>();
+
+            ((ICollection<int>)deque).CopyTo(dist, 0);
+            dist.Should().Equal(new[] { -1, -1, -1, -1, -1, -1, -1, -1 });
+
+            deque.AddLast(1);
+            deque.AddLast(2);
+            ((ICollection<int>)deque).CopyTo(dist, 0);
+            dist.Should().Equal(new[] { 1, 2, -1, -1, -1, -1, -1, -1 });
+
+            deque.AddFirst(3);
+            deque.AddFirst(4);
+            ((ICollection<int>)deque).CopyTo(dist, 0);
+            dist.Should().Equal(new[] { 4, 3, 1, 2, -1, -1, -1, -1 });
+        }
+
+        [Fact]
+        public void Enumerate()
+        {
+            var deque = new Deque<int> { 1, 2, 3, 4, 5 };
+            deque.Should().Equal(new[] { 1, 2, 3, 4, 5 });
+            deque.Reversed().Should().Equal(new[] { 5, 4, 3, 2, 1 });
+        }
+    }
+}
---
+++ AtCoderLibrary/STL/Deque.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using AtCoder.Internal;
+
+namespace AtCoder
+{
+    [DebuggerTypeProxy(typeof(CollectionDebugView<>))]
+    [DebuggerDisplay("Count = {" + nameof(Count) + "}")]
+    public class Deque<T> : IEnumerable<T>, IReadOnlyCollection<T>, ICollection<T>
+    {
+        internal T[] data;
+        internal int mask;
+        internal int head;
+        internal int tail;
+
+        public Deque() : this(8) { }
+        public Deque(int capacity)
+        {
+            if (capacity <= 8) capacity = 8;
+            else capacity = 1 << (InternalBit.CeilPow2(capacity));
+            data = new T[capacity];
+            mask = capacity - 1;
+        }
+
+        public int Count => (tail - head) & mask;
+
+        public T this[int i] => data[(head + i) & mask];
+        public T First => data[head];
+        public T Last => data[(tail - 1) & mask];
+        private static void ThrowDequeIsEmpty() => throw new InvalidOperationException("deque is empty");
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T PopFirst()
+        {
+            if (head == tail) ThrowDequeIsEmpty();
+            var item = data[head];
+            head = (head + 1) & mask;
+            return item;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T PopLast()
+        {
+            if (head == tail) ThrowDequeIsEmpty();
+            return data[tail = (tail - 1) & mask];
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void AddFirst(T item)
+        {
+            data[head = (head - 1) & mask] = item;
+            if (head == tail) Resize();
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void AddLast(T item)
+        {
+            data[tail] = item;
+            tail = (tail + 1) & mask;
+            if (head == tail) Resize();
+        }
+
+        private void Resize()
+        {
+            var oldSize = data.Length;
+            var newArray = new T[oldSize << 1];
+
+            var hsize = oldSize - head;
+            Array.Copy(data, head, newArray, 0, hsize);
+            Array.Copy(data, 0, newArray, hsize, tail);
+            data = newArray;
+            mask = data.Length - 1;
+            head = 0;
+            tail = oldSize;
+        }
+        public void Clear() => head = tail = 0;
+
+        public void Add(T item) => AddLast(item);
+        bool ICollection<T>.Contains(T item)
+        {
+            if (head == tail) return false;
+
+            if (head < tail)
+            {
+                var ix = Array.IndexOf(data, item, head);
+                return ix >= 0 && ix < tail;
+            }
+            else
+            {
+                var ix = Array.IndexOf(data, item, head);
+                if (ix >= 0) return true;
+                ix = Array.IndexOf(data, item);
+                return (uint)ix < tail;
+            }
+
+        }
+
+        void ICollection<T>.CopyTo(T[] array, int arrayIndex)
+        {
+            if (head <= tail)
+            {
+                Array.Copy(data, head, array, arrayIndex, tail - head);
+            }
+            else
+            {
+                var hsize = data.Length - head;
+                Array.Copy(data, head, array, arrayIndex, hsize);
+                Array.Copy(data, 0, array, arrayIndex + hsize, tail);
+            }
+        }
+        public Enumerator Reversed() => new Enumerator(this, true);
+        public Enumerator GetEnumerator() => new Enumerator(this, false);
+
+        bool ICollection<T>.IsReadOnly => false;
+        bool ICollection<T>.Remove(T item) { throw new NotSupportedException(); }
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() => GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        public struct Enumerator : IEnumerator<T>, IEnumerable<T>
+        {
+            readonly Deque<T> deque;
+            readonly bool isReverse;
+            int index;
+            public readonly int last;
+            public T Current => deque.data[index];
+            internal Enumerator(Deque<T> deque, bool isReverse)
+            {
+                this.deque = deque;
+                this.isReverse = isReverse;
+                if (isReverse)
+                {
+                    index = deque.tail;
+                    last = deque.head;
+                }
+                else
+                {
+                    index = deque.head - 1;
+                    last = (deque.tail - 1) & deque.mask;
+                }
+            }
+            object IEnumerator.Current => Current;
+            public bool MoveNext()
+            {
+                if (index == last) return false;
+                if (isReverse) --index; else ++index;
+                index &= deque.mask;
+                return true;
+            }
+            public void Reset() { throw new NotSupportedException(); }
+            public Enumerator GetEnumerator() => this;
+            IEnumerator<T> IEnumerable<T>.GetEnumerator() => this;
+            IEnumerator IEnumerable.GetEnumerator() => this;
+            public void Dispose() { }
+        }
+    }
+
+}
