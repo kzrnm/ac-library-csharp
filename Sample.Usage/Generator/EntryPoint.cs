4466480c5a755219921dc192ef844acbd24fbabc
naminodarie <gengesa@gmail.com>
2020-10-15 01:31:17 +0900

    update sample

--- Sample.Usage/Generator/EntryPoint.cs
+++ Sample.Usage/Generator/EntryPoint.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -12,11 +13,17 @@ class EntryPoint
         Program.Main();
         //var files = Expanded.Expanded.Files; // can build but error CS0103
 
-        var files = (IReadOnlyDictionary<string, string>)Type.GetType("Expanded.Expanded").GetProperty("Files", BindingFlags.Public | BindingFlags.Static).GetValue(null);
+        var files = (dynamic)Type.GetType("Expanded.Expanded").GetProperty("Files", BindingFlags.Public | BindingFlags.Static).GetValue(null);
 
-        var expanded = files.First(p => p.Key.EndsWith("Program.cs")).Value;
+        foreach (var p in files)
+        {
+            if (p.Key.EndsWith("Program.cs"))
+            {
+                File.WriteAllText(GetCurrentPath().Replace("EntryPoint.cs", "Combined.csx"), p.Value.Code);
+            }
+        }
+
         //Console.Error.WriteLine(expanded);
-        File.WriteAllText(GetCurrentPath().Replace("EntryPoint.cs", "Combined.csx"), expanded);
     }
 
     static string GetCurrentPath([CallerFilePath] string path = "") => path;
