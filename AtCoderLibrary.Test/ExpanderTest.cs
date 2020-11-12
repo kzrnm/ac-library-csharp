dc8410f1c9cc4b7f18a8bbc78a0ad52d1c565838
naminodarie <gengesa@gmail.com>
2020-11-12 14:36:09 +0900

    add ExpanderTest

---
+++ AtCoderLibrary.Test/ExpanderTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+namespace AtCoder
+{
+    public class ExpanderTest
+    {
+        [Fact]
+        public void FenwickTree()
+        {
+            var fileinfos = SourceExpander.EmbeddedGenerator.ModuleInitializer.sourceFileInfos;
+            var fwSource = fileinfos.Should()
+                .ContainSingle(f => f.FileName == @"AtCoderLibrary>DataStructure\FenwickTree.cs")
+                .Which;
+            fwSource.Dependencies.Should().BeEquivalentTo(new string[] {
+                @"AtCoderLibrary>Algebra\NumOperator.cs",
+                @"AtCoderLibrary>Util\DebugUtil.cs",
+                @"AtCoderLibrary>Internal\InternalBit.cs",
+            });
+            fwSource.TypeNames.Should().Contain("AtCoder.FenwickTree<TValue, TOp>");
+        }
+    }
+}
