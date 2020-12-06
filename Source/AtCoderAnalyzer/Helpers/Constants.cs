989aadb1acfbd6dc5e833b5ab6e4655e20dd5aae
naminodarie <gengesa@gmail.com>
2020-12-06 21:59:59 +0900

    dotnet-format

--- Source/AtCoderAnalyzer/Helpers/Constants.cs
+++ Source/AtCoderAnalyzer/Helpers/Constants.cs
@@ -2,9 +2,9 @@
 {
     internal static class Constants
     {
-        public const string System_Runtime_CompilerServices 
+        public const string System_Runtime_CompilerServices
             = "System.Runtime.CompilerServices";
-        public const string System_Runtime_CompilerServices_MethodImplAttribute 
+        public const string System_Runtime_CompilerServices_MethodImplAttribute
             = "System.Runtime.CompilerServices.MethodImplAttribute";
 
         public const string AtCoder_IsOperatorAttribute
