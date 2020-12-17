ff139ae7c91bc3ca7b37704b4d4f75aa8ffb5a19
naminodarie <gengesa@gmail.com>
2020-12-18 03:09:23 +0900

    Test Embedded code

--- Test/AtCoderLibrary.Test/SourceExpanderTest.cs
+++ Test/AtCoderLibrary.Test/SourceExpanderTest.cs
@@ -1,52 +1,40 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.IO.Compression;
 using System.Linq;
-using System.Reflection;
 using System.Text.Json;
+using System.Threading.Tasks;
 using FluentAssertions;
-using Kzrnm.Convert.Base32768;
+using SourceExpander;
 using Xunit;
 
 namespace AtCoder.Embedding
 {
     public class SourceExpanderTest
     {
-        private readonly Dictionary<string, string> assemblyMetadata
-            = typeof(IntFenwickTree).Assembly
-            .GetCustomAttributes<AssemblyMetadataAttribute>()
-            .ToDictionary(attr => attr.Key, attr => attr.Value);
-
         [Fact]
-        public void LanguageVersion()
-            => assemblyMetadata.Should().ContainKey("SourceExpander.EmbeddedLanguageVersion")
-                .WhichValue.Should().Be("8.0");
-
-        [Fact]
-        public async void EmbeddedSource()
+        public async Task LanguageVersion()
         {
-            var embeddedSourceGZipJson = Base32768.Decode(assemblyMetadata.Should().ContainKey("SourceExpander.EmbeddedSourceCode.GZipBase32768").WhichValue);
-            using var msIn = new MemoryStream(embeddedSourceGZipJson);
-            using var gzipStream = new GZipStream(msIn, CompressionMode.Decompress);
-            var embeddedSource = await JsonSerializer.DeserializeAsync<SourceFileInfo[]>(gzipStream);
-            embeddedSource.Select(s => s.FileName)
-                .Should()
-                .OnlyContain(name => name.StartsWith("AtCoderLibrary>"));
-            embeddedSource.SelectMany(s => s.TypeNames)
-                .Should()
-                .Contain(new[] {
-                    "AtCoder.IArithmeticOperator<T>",
-                    "AtCoder.FenwickTree<TValue, TOp>",
-                });
+            var embedded = await EmbeddedData.LoadFromAssembly(typeof(Mod1000000007));
+            embedded.AssemblyMetadatas
+                .Should().ContainKey("SourceExpander.EmbeddedLanguageVersion")
+                .WhichValue.Should().Be("8.0");
         }
 
-        private class SourceFileInfo
+        [Fact]
+        public async Task EmbeddedSource()
         {
-            public string FileName { get; set; }
-            public IEnumerable<string> TypeNames { get; set; }
-            //public IEnumerable<string> Usings { get; set; }
-            //public IEnumerable<string> Dependencies { get; set; }
-            //public string CodeBody { get; set; }
+            var embedded = await EmbeddedData.LoadFromAssembly(typeof(Mod1000000007));
+            embedded.AssemblyMetadatas.Should().ContainKey("SourceExpander.EmbeddedSourceCode.GZipBase32768");
+            embedded.SourceFiles.Select(s => s.FileName)
+                .Should()
+                .HaveCountGreaterThan(2)
+                .And
+                .OnlyContain(name => name.StartsWith("AtCoderLibrary>"));
+            embedded.SourceFiles.SelectMany(s => s.TypeNames)
+                .Should()
+                .Contain(
+                    "AtCoder.IArithmeticOperator<T>",
+                    "AtCoder.FenwickTree<TValue, TOp>");
         }
     }
 }
