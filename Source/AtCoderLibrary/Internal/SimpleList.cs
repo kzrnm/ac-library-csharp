ec4c4a207299053c2be0845290dd05a1eaddd5e1
naminodarie <gengesa@gmail.com>
2021-01-10 17:42:09 +0900

    Add SimpleList<T>.Sort(), SimpleList<T>.Reverse()

--- Source/AtCoderLibrary/Internal/SimpleList.cs
+++ Source/AtCoderLibrary/Internal/SimpleList.cs
@@ -42,10 +42,37 @@ namespace AtCoder.Internal
             if (--Count < 0)
                 ThrowIndexOutOfRangeException();
         }
+        public SimpleList<T> Reverse()
+        {
+            Array.Reverse(data, 0, Count);
+            return this;
+        }
+        public SimpleList<T> Reverse(int index, int count)
+        {
+            Array.Reverse(data, index, count);
+            return this;
+        }
+        public SimpleList<T> Sort()
+        {
+            Array.Sort(data, 0, Count);
+            return this;
+        }
+        public SimpleList<T> Sort(IComparer<T> comparer)
+        {
+            Array.Sort(data, 0, Count, comparer);
+            return this;
+        }
+        public SimpleList<T> Sort(int index, int count, IComparer<T> comparer)
+        {
+            Array.Sort(data, index, count, comparer);
+            return this;
+        }
         public void Clear() => Count = 0;
         public bool Contains(T item) => IndexOf(item) >= 0;
         public int IndexOf(T item) => Array.IndexOf(data, item, 0, Count);
         public void CopyTo(T[] array, int arrayIndex) => Array.Copy(data, 0, array, arrayIndex, Count);
+
+        #region Interface
         bool ICollection<T>.IsReadOnly => false;
         T IList<T>.this[int index] { get => data[index]; set => data[index] = value; }
         T IReadOnlyList<T>.this[int index] { get => data[index]; }
@@ -59,6 +86,8 @@ namespace AtCoder.Internal
             for (int i = 0; i < Count; i++)
                 yield return data[i];
         }
+        #endregion Interface
+
         public Span<T>.Enumerator GetEnumerator() => AsSpan().GetEnumerator();
         private static void ThrowIndexOutOfRangeException() => throw new IndexOutOfRangeException();
     }
---
+++ Test/AtCoderLibrary.Test/Internal/SimpleListTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace AtCoder.Internal
+{
+    public class SimpleListTest
+    {
+        [Fact]
+        public void AddAndRemove()
+        {
+            var list = new SimpleList<int>
+            {
+                1,2,3,4,5,6
+            };
+            list.Should().Equal(1, 2, 3, 4, 5, 6);
+            list.RemoveLast();
+            list.Should().Equal(1, 2, 3, 4, 5);
+            list.Add(7);
+            list.Should().Equal(1, 2, 3, 4, 5, 7);
+        }
+
+        [Fact]
+        public void Reverse()
+        {
+            var list = new SimpleList<int>
+            {
+                1,2,3,4,5,6
+            };
+            list.Reverse();
+            list.Should().Equal(6, 5, 4, 3, 2, 1);
+            list.Reverse(1, 5);
+            list.Should().Equal(6, 1, 2, 3, 4, 5);
+            list.RemoveLast();
+            list.Reverse();
+            list.Should().Equal(4, 3, 2, 1, 6);
+        }
+
+
+        [Fact]
+        public void Sort()
+        {
+            var list = new SimpleList<int>
+            {
+                2,3,4,1,5,6
+            };
+            list.Sort();
+            list.Should().Equal(1, 2, 3, 4, 5, 6);
+
+            list = new SimpleList<int>
+            {
+                2,3,4,1,5,6
+            };
+            list.Sort(Comparer<int>.Create((a, b) => b.CompareTo(a)));
+            list.Should().Equal(6, 5, 4, 3, 2, 1);
+
+            list = new SimpleList<int>
+            {
+                2,3,4,1,5,6
+            };
+            list.Sort(1, 3, Comparer<int>.Create((a, b) => b.CompareTo(a)));
+            list.Should().Equal(2, 4, 3, 1, 5, 6);
+        }
+    }
+}
