3bb303f1d866b4064c43bce31fa191afcfb0448d
naminodarie <gengesa@gmail.com>
2021-01-12 02:48:36 +0900

    Convolution solver

---
+++ Test/AtCoderLibrary.LibraryChecker/Solvers/Math/ConvolutionSolver.cs
@@ -0,0 +1,18 @@
+﻿using Kzrnm.Competitive.IO;
+
+namespace AtCoder.Solvers.MathN
+{
+    public class ConvolutionSolver : ISolver
+    {
+        public string Name => "convolution_mod";
+
+        public void Solve(ConsoleReader cr, ConsoleWriter cw)
+        {
+            int n = cr;
+            int m = cr;
+            int[] a = cr.Repeat(n);
+            int[] b = cr.Repeat(m);
+            cw.WriteLineJoin(MathLib.Convolution<Mod998244353>(a, b));
+        }
+    }
+}
