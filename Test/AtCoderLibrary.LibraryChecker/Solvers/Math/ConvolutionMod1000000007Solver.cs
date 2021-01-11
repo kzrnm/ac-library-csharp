446dd1f205855c74a4622637f04a58e8fb089a5d
naminodarie <gengesa@gmail.com>
2021-01-12 02:14:49 +0900

    Invalidate convolution solvers

--- Test/AtCoderLibrary.LibraryChecker/Solvers/Math/ConvolutionMod1000000007Solver.cs
+++ Test/AtCoderLibrary.LibraryChecker/Solvers/Math/ConvolutionMod1000000007Solver.cs
@@ -2,17 +2,17 @@
 
 namespace AtCoder.Solvers.MathN
 {
-    public class ConvolutionMod1000000007Solver : ISolver
-    {
-        public string Name => "convolution_mod_1000000007";
+    //public class ConvolutionMod1000000007Solver : ISolver
+    //{
+    //    public string Name => "convolution_mod_1000000007";
 
-        public void Solve(ConsoleReader cr, ConsoleWriter cw)
-        {
-            int n = cr;
-            int m = cr;
-            int[] a = cr.Repeat(n);
-            int[] b = cr.Repeat(m);
-            cw.WriteLineJoin(MathLib.Convolution<Mod1000000007>(a, b));
-        }
-    }
+    //    public void Solve(ConsoleReader cr, ConsoleWriter cw)
+    //    {
+    //        int n = cr;
+    //        int m = cr;
+    //        int[] a = cr.Repeat(n);
+    //        int[] b = cr.Repeat(m);
+    //        cw.WriteLineJoin(MathLib.Convolution<Mod1000000007>(a, b));
+    //    }
+    //}
 }
--- Test/AtCoderLibrary.LibraryChecker/Solvers/Math/ConvolutionMod998244353Solver.cs
+++ Test/AtCoderLibrary.LibraryChecker/Solvers/Math/ConvolutionMod998244353Solver.cs
@@ -2,17 +2,17 @@
 
 namespace AtCoder.Solvers.MathN
 {
-    public class ConvolutionMod998244353Solver : ISolver
-    {
-        public string Name => "convolution_mod";
+    //public class ConvolutionMod998244353Solver : ISolver
+    //{
+    //    public string Name => "convolution_mod";
 
-        public void Solve(ConsoleReader cr, ConsoleWriter cw)
-        {
-            int n = cr;
-            int m = cr;
-            int[] a = cr.Repeat(n);
-            int[] b = cr.Repeat(m);
-            cw.WriteLineJoin(MathLib.Convolution<Mod998244353>(a, b));
-        }
-    }
+    //    public void Solve(ConsoleReader cr, ConsoleWriter cw)
+    //    {
+    //        int n = cr;
+    //        int m = cr;
+    //        int[] a = cr.Repeat(n);
+    //        int[] b = cr.Repeat(m);
+    //        cw.WriteLineJoin(MathLib.Convolution<Mod998244353>(a, b));
+    //    }
+    //}
 }
