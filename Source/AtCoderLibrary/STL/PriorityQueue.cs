5bac610fc90d351b3857a2f6122ad0532632a604
kzrnm <gengesa@gmail.com>
2021-09-14 00:45:39 +0900

    Optimize PriorityQueue

--- Source/AtCoderLibrary/STL/PriorityQueue.cs
+++ Source/AtCoderLibrary/STL/PriorityQueue.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using AtCoder.Internal;
 
 namespace AtCoder
 {
     [DebuggerDisplay("Count = {" + nameof(Count) + "}")]
-    public class PriorityQueue<T> : PriorityQueueOp<T, IComparer<T>>
+    public class PriorityQueue<T> : PriorityQueueOp<T, ComparableComparer<T>>
+        where T : IComparable<T>
     {
-        public PriorityQueue() : base(Comparer<T>.Default) { }
-        public PriorityQueue(int capacity) : base(capacity, Comparer<T>.Default) { }
-        public PriorityQueue(IComparer<T> comparer) : base(comparer) { }
-        public PriorityQueue(int capacity, IComparer<T> comparer) : base(capacity, comparer) { }
+        public PriorityQueue() : base(new ComparableComparer<T>()) { }
+        public PriorityQueue(int capacity) : base(capacity, new ComparableComparer<T>()) { }
+        public PriorityQueue(IComparer<T> comparer) : base(new ComparableComparer<T>(comparer)) { }
+        public PriorityQueue(int capacity, IComparer<T> comparer) : base(capacity, new ComparableComparer<T>(comparer)) { }
     }
 }
--- Source/AtCoderLibrary/STL/PriorityQueue`2.cs
+++ Source/AtCoderLibrary/STL/PriorityQueue`2.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using AtCoder.Internal;
 
 namespace AtCoder
 {
     [DebuggerDisplay("Count = {" + nameof(Count) + "}")]
-    public class PriorityQueueDictionary<TKey, TValue> : PriorityQueueOp<TKey, TValue, IComparer<TKey>>
+    public class PriorityQueueDictionary<TKey, TValue> : PriorityQueueOp<TKey, TValue, ComparableComparer<TKey>>
+        where TKey : IComparable<TKey>
     {
-        public PriorityQueueDictionary() : base(Comparer<TKey>.Default) { }
-        public PriorityQueueDictionary(int capacity) : base(capacity, Comparer<TKey>.Default) { }
-        public PriorityQueueDictionary(IComparer<TKey> comparer) : base(comparer) { }
-        public PriorityQueueDictionary(int capacity, IComparer<TKey> comparer) : base(capacity, comparer) { }
+        public PriorityQueueDictionary() : base(new ComparableComparer<TKey>()) { }
+        public PriorityQueueDictionary(int capacity) : base(capacity, new ComparableComparer<TKey>()) { }
+        public PriorityQueueDictionary(IComparer<TKey> comparer) : base(new ComparableComparer<TKey>(comparer)) { }
+        public PriorityQueueDictionary(int capacity, IComparer<TKey> comparer) : base(capacity, new ComparableComparer<TKey>(comparer)) { }
     }
 }
