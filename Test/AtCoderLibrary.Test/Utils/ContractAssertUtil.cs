53fdfc839395c1a5da9e65b94094fecbed7b14ce
kzrnm <gengesa@gmail.com>
2021-10-16 19:10:21 +0900

    Update library

--- Test/AtCoderLibrary.Test/Utils/ContractAssertUtil.cs
+++ Test/AtCoderLibrary.Test/Utils/ContractAssertUtil.cs
@@ -6,10 +6,11 @@ namespace AtCoder
 {
     public static class ContractAssertUtil
     {
-        public static void ThrowContractAssert<TDelegate>(
-            this DelegateAssertions<TDelegate> assertions,
+        public static void ThrowContractAssert<TDelegate, TAssertions>(
+            this DelegateAssertions<TDelegate, TAssertions> assertions,
             string because = "",
-            params object[] becauseArgs) where TDelegate : Delegate
+            params object[] becauseArgs)
+            where TDelegate : Delegate where TAssertions : DelegateAssertions<TDelegate, TAssertions>
         {
             assertions.Throw<ContractAssertException>(because, becauseArgs);
         }
