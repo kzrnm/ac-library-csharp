79f0ef6d0905252f2961f3b0cfd583cc8df447c6
kzrnm <gengesa@gmail.com>
2022-06-13 21:48:40 +0900

    FenwickTree<T>

--- Source/AtCoderLibrary/DataStructure/Wrappers/StaticModIntFenwickTree.cs
+++ Source/AtCoderLibrary/DataStructure/Wrappers/StaticModIntFenwickTree.cs
@@ -12,5 +12,8 @@
     /// </list>
     /// <para>を O(log⁡N) で求めることが出来るデータ構造です。</para>
     /// </summary>
+#if GENERIC_MATH
+    [System.Obsolete("Use generic math")]
+#endif
     public class StaticModIntFenwickTree<T> : FenwickTree<StaticModInt<T>, StaticModIntOperator<T>> where T : struct, IStaticMod { public StaticModIntFenwickTree(int n) : base(n) { } }
 }
