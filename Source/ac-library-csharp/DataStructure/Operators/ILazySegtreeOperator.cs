9ad7ca144f92f78056b17e87ab89ea666e6255bb
kzrnm <gengesa@gmail.com>
2023-12-17 02:45:27 +0900

    ILazySegtreeOperator<T, F> : ISegtreeOperator<T>

--- Source/ac-library-csharp/DataStructure/Operators/ILazySegtreeOperator.cs
+++ Source/ac-library-csharp/DataStructure/Operators/ILazySegtreeOperator.cs
@@ -1,25 +1,17 @@
 ﻿namespace AtCoder
 {
     /// <summary>
-    /// モノイドを定義するインターフェイスです。
+    /// モノイドと写像を定義するインターフェイスです。
     /// </summary>
     /// <typeparam name="T">操作を行う型。</typeparam>
     /// <typeparam name="F">写像の型。</typeparam>
     [IsOperator]
-    public interface ILazySegtreeOperator<T, F>
+    public interface ILazySegtreeOperator<T, F> : ISegtreeOperator<T>
     {
-        /// <summary>
-        /// Operate(x, <paramref name="Identity"/>) = x を満たす単位元。
-        /// </summary>
-        T Identity { get; }
         /// <summary>
         /// Mapping(<paramref name="FIdentity"/>, x) = x を満たす恒等写像。
         /// </summary>
         F FIdentity { get; }
-        /// <summary>
-        /// 結合律 Operate(Operate(a, b), c) = Operate(a, Operate(b, c)) を満たす写像。
-        /// </summary>
-        T Operate(T x, T y);
         /// <summary>
         /// 写像　<paramref name="f"/> を <paramref name="x"/> に作用させる関数。
         /// </summary>
